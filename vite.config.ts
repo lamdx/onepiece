8b99d24940611579d7a1aa1c0afa5861e1d444c3
lamdx <513256514@qq.com>
2022-07-07 17:10:54 +0800

    feat: 在开发环境下启动时自动在浏览器中打开应用程序

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,9 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [vue()],
+  server: {
+    // 在开发环境下启动时自动在浏览器中打开应用程序
+    open: true
+  }
 })
